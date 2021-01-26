704aa0aa9a2447d13541ea566d8f17ed02e59a87
CodyPumpkin <172173327@qq.com>
2021-01-26 17:34:34 +0800

    update footer

--- footer.js
+++ footer.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import '../styles/_footer.css';
 
 export default class Footer extends Component {
     componentDidMount() {
@@ -16,7 +17,7 @@ export default class Footer extends Component {
     getFooterPostionStyle = () => {
       this.footer.classList.remove("footerPosi")
       let hei = window.innerHeight //整个窗口的高度
-      let footerTop = this.footer.offsetTop //底部footer相对于顶部的距离
+      let footerTop = this.footer.offsetTop//底部footer相对于顶部的距离
       let footerHei = this.footer.offsetHeight
       if (footerTop+footerHei>hei) {
         this.footer.classList.remove("footerPosi")
@@ -28,8 +29,8 @@ export default class Footer extends Component {
     render() {
         return (
             <div className="site-footer" ref={(o)=>this.footer=o}>
-            <p className="text-center">Made with 💗 in Melbourne.</p>
-            <p className="text-center">© 2015 Broccoli & Co. All rights reserved.</p>
+            <p className="text-center">Made with 💗 in Melbourne.</p >
+            <p className="text-center">© 2015 Broccoli & Co. All rights reserved.</p >
             </div>
         )
       }
